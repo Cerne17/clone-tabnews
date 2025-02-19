8b3c72c67152f6ae5f3ff9ec103810025cae86de
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-02-19 13:25:18 -0300

    feat: changed the text inside the main page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function Home() {
   return <>
- <h1>Bom dia, caros devs!</h1>
- <p>Estou começando minha jornada com Next.js e React.js 👋</p>
- </>;
+    <h1>Bom dia, caros devs!</h1>
+    <p>Estou começando minha jornada com Next.js e React.js 👋</p>
+    <p>Meu Github: https://github.com/Cerne17</p>
+  </>;
 }
 
 export default Home;
