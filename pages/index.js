942b07a3d3084471c52d905736d293bb6b5c36fd
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-02-20 03:45:43 -0300

    feat(linting): adds `lint:check` and `lint:fix` npm commands

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 function Home() {
-  return <>
-    <h1>Bom dia, caros devs!</h1>
-    <p>Estou começando minha jornada com Next.js e React.js 👋</p>
-    <p>Meu Github: https://github.com/Cerne17</p>
-  </>;
+  return (
+    <>
+      <h1>Bom dia, caros devs!</h1>
+      <p>Estou começando minha jornada com Next.js e React.js 👋</p>
+      <p>Meu Github: https://github.com/Cerne17</p>
+    </>
+  );
 }
 
 export default Home;
