ceddf023092f61002b04d2014c215ad3a56b6f47
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-02-28 15:28:54 -0300

    fix: fixes the parameterized query - no SQL Injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,5 @@
 import database from "infra/database.js"
+// import database from "../../../../infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
@@ -9,9 +10,11 @@ async function status(request, response) {
   const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
   const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
